e0abff1ac3e3a05f20ecd3b01f9c8f081d20116c
VEXITYa <45267014+VEXITYa@users.noreply.github.com>
2022-11-23 01:00:36 +0400

    Add files via upload
    Functional added(rn doesnt work)

--- CourseworkC++.cpp
+++ CourseworkC++.cpp
@@ -5,34 +5,42 @@
 #include <exception>
 #include <iomanip>
 #include <sstream>
+#include <cmath>
+#include <algorithm>
 
 class FrequencyRatio
 {
-    char delim = ';';
+    char delim = ' ';
     std::string path;
-    std::vector<double> first_frequency;
-    std::vector<double> second_frequency;
-    std::vector<double> times;
-    std::vector<double> first_derivatives;
-    std::vector<double> second_derivatives;
+    std::vector<long double> first_frequency;
+    std::vector<long double> second_frequency;
+    std::vector<long double> times;
+    std::vector<long double> first_derivatives;
+    std::vector<long double> second_derivatives;
+    std::vector<long double> first_phases;
+    std::vector<long double> second_phases;
+    bool with_time = false;
 
-    void SplitString(std::string line, bool with_time=false)
+    void SplitString(std::string line)
     {
         auto first_delim_index = line.find_first_of(delim);
         auto second_delim_index = line.find_last_of(delim);
-        double first = std::stod(line.substr(0, first_delim_index));
-        double second = std::stod(line.substr(first_delim_index + 1, second_delim_index));
+        long double first = std::stod(line.substr(0, first_delim_index));
+        long double second = std::stod(line.substr(first_delim_index + 1, second_delim_index));
         first_frequency.push_back(first);
         second_frequency.push_back(second);
         if (with_time)
         {
-            double time = std::stod(line.substr(second_delim_index + 1, line.size() - second_delim_index));
+            long double time = std::stod(
+                line.substr(second_delim_index + 1, line.size() - second_delim_index)
+            );
             times.push_back(time);
         }
     }
 public:
-    FrequencyRatio(std::string path)
+    FrequencyRatio(std::string path, bool with_time=false)
     {
+        this->with_time = with_time;
         std::ifstream file_in(path.c_str());
         if (file_in.bad())
         {
@@ -50,31 +58,62 @@ public:
         }
         file_in.close();
     }
-    void FindDerivatives(bool with_time=false)
+    void FindDerivatives()
     {
         for (long i = 1; i < first_frequency.size(); i++)
         {
             first_derivatives.push_back(
-                (first_frequency[i] - first_frequency[i - 1])/
-                ( with_time ? times[i] - times[i - 1] : 1.)
+                (first_frequency[i] - first_frequency[i - 1]) /
+                (with_time ? times[i] - times[i - 1] : (long double)1)
             );
             second_derivatives.push_back(
                 (second_frequency[i] - second_frequency[i - 1]) /
-                (with_time ? times[i] - times[i - 1] : 1.)
+                (with_time ? times[i] - times[i - 1] : (long double)1)
             );
+
+        }
+    }
+
+    void CalculatePhases()
+    {
+        for (long i = 0; i < first_derivatives.size(); i++)
+        {
+            first_phases.push_back(atan2(first_derivatives[i], first_frequency[i + 1]));
+            second_phases.push_back(atan2(second_derivatives[i], second_frequency[i + 1]));
+        }
+    }
+
+    void PhasesOutput(std::string output_path)
+    {
+        std::ofstream file_out((output_path).c_str());
+        for (int i = 0; i < first_phases.size(); i++)
+        {
+            file_out << std::fixed << std::setprecision(30) << first_phases[i] << ' ' << second_phases[i] << std::endl;
+        }
+        std::cout << std::fixed << std::setprecision(30) << first_phases[0] << ' ' << second_phases[0] << std::endl;
+        file_out.close();
+    }
+
+    void DerivativesOutput(std::string output_path)
+    {
+        std::ofstream file_out((output_path).c_str());
+        for (int i = 0; i < first_derivatives.size(); i++)
+        {
+            file_out << std::fixed << std::setprecision(30) << first_derivatives[i] << ' ' << second_derivatives[i] << std::endl;
         }
 
-        for (auto x : first_derivatives)
-        {
-            std::cout << x << std::endl;
-        }
+        file_out.close();
     }
 };
 
 int main()
 {
-    std::string path = "C:/Users/VEX1T/Documents/test1.csv";
-    FrequencyRatio test1(path);
+    std::string path = "C:/Users/VEX1T/Documents/data.txt";
+    std::string output_path = "C:/Users/VEX1T/Documents/data_out.txt";
+    FrequencyRatio test1(path, true);
     test1.FindDerivatives();
+    test1.CalculatePhases();
+    test1.PhasesOutput(output_path);
+    test1.DerivativesOutput("C:/Users/VEX1T/Documents/data_outderiv.txt");
 }
 
